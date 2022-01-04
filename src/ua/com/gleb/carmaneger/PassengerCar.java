54eeeb51590982dcdae1d82c6c811cb429c0b887
korSliker <gkorobov8@gmail.com>
2022-01-04 15:57:48 +0200

    Extend Car

---
+++ src/ua/com/gleb/carmaneger/PassengerCar.java
@@ -0,0 +1,7 @@
+package ua.com.gleb.carmaneger;
+
+public class PassengerCar extends Car{
+    public PassengerCar(String name, int yearOfProduction, int price, int weight, Color color) {
+        super(name, yearOfProduction, price, weight, color);
+    }
+}
